16743a9d817ad900f036117ccb7c4540bea9a743
Daniel Ibañez <daniel.ibanez@softonic.com>
2016-08-31 13:18:50 +0200

    BUG: Delete all environemnts instead of specific

--- docker/jenkins-executor/bin/src/deploy.sh
+++ docker/jenkins-executor/bin/src/deploy.sh
@@ -82,7 +82,8 @@ function deploy {
 function deployed_version {
     dcos marathon group list --json \
     | jq ".[]?.groups[]?.groups[]?.groups[]? \
-    | [select(.apps[].labels.stack_environment != \"$PROJECT_GROUP/$PROJECT_NAME/$ENV/$PROJECT_VERSION\").apps[].labels.stack_environment]|unique[]"
+    | [select(.apps[].labels.stack_environment == \"$PROJECT_GROUP/$PROJECT_NAME/$ENV\").apps[].labels.stack_version]|unique \
+    | select(.[] != \"$PROJECT_GROUP/$PROJECT_NAME/$PROJECT_VERSION\")[]"
 }
 
 function deploy_internal_stack {
